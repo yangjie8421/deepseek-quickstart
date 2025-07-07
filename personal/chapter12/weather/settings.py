6255d967860616feffc656da79fa7fa8837c32f3
yangjie8421 <yangjie8421@foxmail.com>
2025-07-07 22:53:55 +0800

    chapter 12 homework

---
+++ personal/chapter12/weather/settings.py
@@ -0,0 +1,7 @@
+# 高德天气
+GD_API_BASE = "https://restapi.amap.com/v3/weather/weatherInfo"
+GD_API_KEY  = "4347a9cb1ebdab366f80f9e93cf50633"
+
+# 美国国家气象局 (NWS) API 的基础 URL 和请求头
+NWS_API_BASE = "https://api.weather.gov"
+NWS_USER_AGENT = "weather-app/1.0"
